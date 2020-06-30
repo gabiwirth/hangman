d69e5ae85706208a268fb537fde6e4f6a9debbe1
gabiwirth <gabiwirth99@gmail.com>
2020-06-29 21:57:46 -0700

    finished structure, starting debugging/polishing

--- hangman.py
+++ hangman.py
@@ -1,8 +1,8 @@
 import random
 
-
+# TODO: change to load wordbank w/ this datastructure from a file.
 wordbank = {
-    4: ["poop", "stub"],
+    4: ["poop", "stub", "knob"],
     5: ["jacob", "dumpy"],
     6: ["butter", "animal"]
 }
@@ -19,7 +19,7 @@ def blank_filler(guess,blanks,goal_word):
     return blanks
 
 def display(guess, wrong_guesses, blanks):
-    h1 = ["  O       Wrong guesses:", "/", "|" , '\\','  |', " /", "\\"]
+    h1 = ["     O       Wrong guesses:", "   /", "|" , '\\','     |', "    /", "\\"]
     wrong_blanks = ''
     new_blanks = ' '
     for i in wrong_guesses:
@@ -29,7 +29,9 @@ def display(guess, wrong_guesses, blanks):
     for j in blanks:
         new_blanks += j
         new_blanks += ' '
-
+    for i in range(len(h1)):
+        if i >= len(wrong_guesses):
+           h1[i] = ' '
     # while i < range(len(wrong_blanks)):
         # h1 = ' '
 
@@ -38,3 +40,32 @@ def display(guess, wrong_guesses, blanks):
     print(h1[4])
     print(h1[5], h1[6])
     print(new_blanks)
+
+def main():
+    word_length = input('Enter the length of the word you want (4-6): ')
+    word_length = int(word_length)
+    goal_word = word_picker(word_length)
+    blanks = ['_'] * word_length
+    turns_left = 7
+    wrong_guesses = []
+    while turns_left >= 0:
+        guess = input('Enter your guess: ')
+        old_blanks = blanks.copy()
+        blank_filler(guess,blanks,goal_word)
+        if old_blanks == blanks:
+            wrong_guesses.append(guess)
+            turns_left -= 1
+        display(guess, wrong_guesses, blanks)
+        guesses_string = ''.join(blanks)
+        if guesses_string == goal_word:
+            print('You won the game!')
+            break
+        if turns_left == 0:
+            print('You ran out of guesses! Game over.')
+            play_again = input("Would you like to play again? (yes/no) ")
+            play_again = play_again.lower()
+            if play_again == 'yes':
+                main()
+            else:
+                print('Thanks for playing!')
+                break
